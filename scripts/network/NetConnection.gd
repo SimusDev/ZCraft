ab754495458cfd90f965f5f413c4914f72874a88
zxcvlad78 <danyaps@mail.ru>
2026-06-24 21:25:40 +0300

    Update NetConnection.gd

--- scripts/network/NetConnection.gd
+++ scripts/network/NetConnection.gd
@@ -1,2 +1,30 @@
 extends Node
 class_name NetConnection
+
+signal peer_closed()
+
+var _current_peer:ENetMultiplayerPeer
+
+func get_peer() -> ENetMultiplayerPeer:
+	return _current_peer
+
+func create_peer() -> ENetMultiplayerPeer:
+	return ENetMultiplayerPeer.new()
+
+func close_peer() -> void:
+	_current_peer.close()
+	peer_closed.emit()
+
+func create_server(port: int, max_clients:int = 32) -> Error:
+	close_peer()
+	_current_peer = create_peer()
+	var error = get_peer().create_server(port, max_clients)
+	
+	return error
+
+func create_client(address: String, port: int) -> Error:
+	close_peer()
+	_current_peer = create_peer()
+	var error = get_peer().create_client(address, port)
+	
+	return error
