1cbe476556284dff3cee699732f05e018ff757c0
SimusDev <simusdev@mail.ru>
2026-06-24 23:04:31 +0300

    123

--- scenes/world/game_manager.gd
+++ scenes/world/game_manager.gd
@@ -1,35 +1,42 @@
 extends Node3D
 
-
 @export var player_scene:PackedScene
 @export var players_node:Node3D
-var players = {}
 
 func _ready():
-	# Только сервер (хост) управляет спавном
-	if not multiplayer.is_server():
-		return
+	if multiplayer.is_server():
+		multiplayer.peer_connected.connect(_on_server_player_connected)
+		multiplayer.peer_disconnected.connect(_on_server_player_disconnected)
+		_spawn_player(1)
+	else:
+		_request.rpc_id(1)
 
-	# Подключаем сигналы для обработки подключений/отключений
-	multiplayer.peer_connected.connect(_on_player_connected)
-	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+@rpc("any_peer", "reliable")
+func _request() -> void:
+	for i in players_node.get_children():
+		_spawn_player.rpc_id(multiplayer.get_remote_sender_id(), int(i.name))
+		
 
-	# Спавним игрока для самого хоста (peer_id = 1)
-	_spawn_player(1)
+@rpc("authority", "reliable")
+func _receive(peers: PackedInt32Array) -> void:
+	for pid: int in peers:
+		_spawn_player(pid)
 
-func _on_player_connected(id: int):
-	_spawn_player(id)
+func _on_server_player_connected(peer: int) -> void:
+	_spawn_player.rpc(peer)
 
-func _on_player_disconnected(id: int):
-	# Удаляем игрока, если он все еще есть в дереве
-	var player = players_node.get_node_or_null(str(id))
+func _on_server_player_disconnected(peer: int) -> void:
+	_despawn_player.rpc(peer)
+
+@rpc("authority", "call_local", "reliable")
+func _spawn_player(peer: int) -> void:
+	var instance: Node = player_scene.instantiate()
+	instance.set_multiplayer_authority(peer)
+	instance.name = str(peer)
+	players_node.add_child.call_deferred(instance)
+
+@rpc("authority", "call_local", "reliable")
+func _despawn_player(peer: int) -> void:
+	var player: Node = players_node.get_node_or_null(str(peer))
 	if player:
 		player.queue_free()
-
-func _spawn_player(id: int):
-	# Создаем экземпляр сцены игрока
-	var player = player_scene.instantiate()
-	player.name = str(id)
-	player.set_multiplayer_authority(id, true)
-	# Добавляем в контейнер. Спавнер автоматически реплицирует его на всех клиентов
-	players_node.add_child(player, true) # 'true' для force_readable_name
