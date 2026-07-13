40dd9b567be3dff66ff74921177002daa37f6d3b
SimusDev <simusdev@mail.ru>
2026-07-13 17:32:56 +0300

    Update ZEntity.cs

--- scripts/entity/ZEntity.cs
+++ scripts/entity/ZEntity.cs
@@ -3,6 +3,14 @@ using System;
 
 public partial class ZEntity : Node
 {
-	[Export] Godot.Collections.Array<ZEntityComponent> _components = new();
+    [Export] Godot.Collections.Array<ZEntityComponent> _components = new();
+
+#nullable enable
+    public T FindComponent<T>() where T: ZEntityComponent
+    {
+        PhysicsServer3D.BodyCreate();
+        return null;
+    }
+#nullable disable
 
 }
